0a08868706aa2be05505547b916ad6c11fde4a58
Adam Polak <adamjpolak@gmail.com>
2024-10-26 16:15:18 -0500

    basic tests written for sql sb

--- Backend/WebAPI.Tests/DataAccess/StringBuilders/SqlSBTests.cs
+++ Backend/WebAPI.Tests/DataAccess/StringBuilders/SqlSBTests.cs
@@ -1,4 +1,3 @@
-using Microsoft.Identity.Client;
 using WebAPI.DataAccess.Lib;
 
 namespace WebAPI.Tests.DataAccess.Lib;
@@ -61,12 +60,13 @@ public class SqlSB_DeleteString_ListOfValues : TestSqlSB
     [Fact]
     public void DeleteString_MultipleElements()
     {
+        Random rnd = new Random();
         string label1 = "label1";
-        int value1 = 1;
+        int value1 = rnd.Next();
         string label2 = "label2";
-        string value2 = "2";
+        string value2 = $"{rnd.NextDouble() * rnd.Next()}";
         string label3 = "label3";
-        long value3 = 9282334899;
+        long value3 = rnd.NextInt64();
         IntSqlValue v1 = new IntSqlValue(label1, value1);
         StringSqlValue v2 = new StringSqlValue(label2, value2);
         LongSqlValue v3 = new LongSqlValue(label3, value3);
@@ -103,12 +103,13 @@ public class SqlSB_InsertString : TestSqlSB
     [Fact]
     public void InsertString_MultipleElements()
     {
+        Random rnd = new Random();
         string label1 = "label1";
-        int value1 = 1;
+        int value1 = rnd.Next();
         string label2 = "label2";
-        string value2 = "2";
+        string value2 = $"{rnd.NextDouble() * rnd.Next()}";
         string label3 = "label3";
-        long value3 = 9282334899;
+        long value3 = rnd.NextInt64();
         IntSqlValue v1 = new IntSqlValue(label1, value1);
         StringSqlValue v2 = new StringSqlValue(label2, value2);
         LongSqlValue v3 = new LongSqlValue(label3, value3);
@@ -121,15 +122,73 @@ public class SqlSB_InsertString : TestSqlSB
 
 public class SqlSB_SelectString_SingleValue : TestSqlSB
 {
+    [Fact]
+    public void SelectString_IntValue()
+    {
+        string result = _sb.SelectString(_table, new IntSqlValue("number", 1));
+        Assert.Equal($"SELECT * FROM {_table} WHERE number=1;", result);
+    }
 
+    [Fact]
+    public void SelectString_StringValue()
+    {
+        string label = "str";
+        string value = "test";
+        string result = _sb.SelectString(_table, new StringSqlValue(label, value));
+        string expected = $"SELECT * FROM {_table} WHERE {label}='{value}';";
+        Assert.Equal(expected, result);
+    }
 }
 
 public class SqlSB_SelectString_ListOfValues : TestSqlSB
 {
+    [Fact]
+    public void SelectString_EmptyList_ThrowsException()
+    {
+        Assert.ThrowsAny<Exception>(
+            () => {
+                _sb.SelectString(_table, []);
+            }
+        );
+    }
 
+    [Fact]
+    public void SelectString_SingleElement()
+    {
+        string label = "label";
+        int value = 2;
+        List<ISqlValue> values = [ new IntSqlValue(label, value) ];
+        string result = _sb.SelectString(_table, values);
+        string expected = $"SELECT * FROM {_table} WHERE {label}={value};";
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void SelectString_MultipleElements()
+    {
+        Random rnd = new Random();
+        string label1 = "label1";
+        int value1 = rnd.Next();
+        string label2 = "label2";
+        string value2 = $"{rnd.NextDouble() * rnd.Next()}";
+        string label3 = "label3";
+        long value3 = rnd.NextInt64();
+        IntSqlValue v1 = new IntSqlValue(label1, value1);
+        StringSqlValue v2 = new StringSqlValue(label2, value2);
+        LongSqlValue v3 = new LongSqlValue(label3, value3);
+        List<ISqlValue> values = [ v1, v2, v3 ];
+        string result = _sb.SelectString(_table, values);
+        string expected = $"SELECT * FROM {_table} WHERE {label1}={value1} AND {label2}='{value2}' AND {label3}={value3};";
+        Assert.Equal(expected, result);
+    }
 }
 
 public class SqlSB_SelectString_All : TestSqlSB
 {
-
+    public void SelectString_All()
+    {
+        string result = _sb.SelectAllString(_table);
+        string expected = $"SELECT * FROM {_table};";
+        Assert.Equal(expected, result);
+    }
 }
--- Backend/WebAPI/DataAccess/Lib/StringBuilders/SqlSB.cs
+++ Backend/WebAPI/DataAccess/Lib/StringBuilders/SqlSB.cs
@@ -1,8 +1,15 @@
 
+using Microsoft.IdentityModel.Tokens;
+
 namespace WebAPI.DataAccess.Lib;
 
 public class SqlSB : ISqlStringBuilder
 {
+    private void ValidateList(List<ISqlValue> values)
+    {
+        if(values.IsNullOrEmpty()) throw new Exception("Can't build string with empty list.");
+    }
+
     public string DeleteString(string table, ISqlValue value)
     {
         string sqlStr = $"DELETE FROM {table} WHERE {value.GetSqlEqualsString()};";
@@ -11,6 +18,8 @@ public class SqlSB : ISqlStringBuilder
 
     public string DeleteString(string table, List<ISqlValue> values)
     {
+        ValidateList(values);
+
         string sqlStr = $"DELETE FROM {table} WHERE ";
 
         for(int i = 0; i < values.Count; i++)
@@ -28,6 +37,8 @@ public class SqlSB : ISqlStringBuilder
 
     public string InsertString(string table, List<ISqlValue> values)
     {
+        ValidateList(values);
+
         string sqlStr = $"INSERT INTO {table} (";
 
         for(int i = 0; i < values.Count; i++)
@@ -60,6 +71,8 @@ public class SqlSB : ISqlStringBuilder
 
     public string SelectString(string table, List<ISqlValue> values)
     {
+        ValidateList(values);
+        
         string sqlStr = $"SELECT * FROM {table} WHERE ";
 
         for(int i = 0; i < values.Count; i++)
