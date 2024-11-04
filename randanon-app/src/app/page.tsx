5ca2715f4da42e58e8619cf36152bd519bc1ec4a
Adam Polak <adamjpolak@gmail.com>
2024-11-03 21:41:17 -0600

    fixed document not found error

--- randanon-app/src/app/page.tsx
+++ randanon-app/src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import getUserFromCookies from "./lib/util/cookies";
 import ChatBox from "./lib/ui/chat";
 import { createUser, validateUser } from "./lib/util/userEvents";
 
 export default function Home() {
 
-  const cookieUser = getUserFromCookies(document.cookie);
+  let cookieUser = { ID: 0, UserKey: 0 };
+  useEffect(() => {
+    cookieUser = getUserFromCookies(document.cookie);
+  });
 
   const [user, setUser] = useState(cookieUser);
 
@@ -25,8 +28,10 @@ export default function Home() {
 
   setValidUser();
 
-  document.cookie = `UserID=${user.ID}; Max-Age=86400`;
-  document.cookie = `UserKey=${user.UserKey}; Max-Age=86400`;
+  useEffect(() => {
+    document.cookie = `UserID=${user.ID}; Max-Age=86400`;
+    document.cookie = `UserKey=${user.UserKey}; Max-Age=86400`;
+  });
   
   return (
     <main >
